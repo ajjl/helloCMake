74591743df3b18e3131fbea7d00e7296faeecdf3
Alexander Leinoff <alexander-leinoff@uiowa.edu>
2016-04-07 16:06:30 -0500

    ENH: this works with regular ctest now

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -1,7 +1,7 @@
 set (CTEST_PROJECT_NAME "hello")
 
-set(CTEST_DROP_METHOD "http")
+set(CTEST_DROP_METHOD "https")
 set(CTEST_DROP_SITE "code-testing.iibi.uiowa.edu")
 #set(CTEST_DROP_SITE "172.17.63.93:4080")
-set(CTEST_DROP_LOCATION "/public/submit.php?project=HJtest")
+set(CTEST_DROP_LOCATION "/submit.php?project=HJtest")
 set(CTEST_DROP_SITE_CDASH TRUE)
